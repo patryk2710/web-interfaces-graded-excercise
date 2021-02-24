db38184531338f990104c642c52e2380cf536bef
patryk2710 <patryk2710dd@gmail.com>
2021-02-24 23:37:42 +0200

    completed debugging and testing

--- services/users.js
+++ services/users.js
@@ -2,25 +2,20 @@ const { v4: uuidv4 } = require('uuid');
 
 let users = [
   {
-    id: 1,
     username: 'tester',
     password: '$2y$06$PhZ74dT8/5g6B8SgssFq6ey4ojLxmP6pos2DcevMUGw25Vc9jGEou', // testerpassword
     firstName: 'tester',
-    lastName: 'testing',
-    validApiKey: null
+    lastName: 'testing'
   },
   {
-    id: 2,
     username: "johndoe",
     firstName: 'john',
     lastName: 'doe',
-    password: '$2y$06$eQav1OaIyWSUnlvPSaFXRe5gWRqXd.s9vac1SV1GafxAr8hdmsgCy', // johndoepassword
-    validApiKey: null
+    password: '$2y$06$eQav1OaIyWSUnlvPSaFXRe5gWRqXd.s9vac1SV1GafxAr8hdmsgCy' // johndoepassword
   },
 ];
 
 module.exports = {
-  getUserById: (id) => users.find(u => u.id == id),
   getUserByName: (username) => users.find(u => u.username == username),
   addUser: (username, password, firstName, lastName) => {
     users.push({
